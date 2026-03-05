cc81bd27d55da9784d3964cb21ed5fa91732ae19
soaovo <soao0707@gmail.com>
2026-03-05 19:06:24 +0800

    init

---
+++ build/demos/CMakeFiles/test_acorn.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/cxy/ACORN/ACORN/demos/test_acorn.cpp" "demos/CMakeFiles/test_acorn.dir/test_acorn.cpp.o" "gcc" "demos/CMakeFiles/test_acorn.dir/test_acorn.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/cxy/ACORN/ACORN/build/faiss/CMakeFiles/faiss.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/faiss/CMakeFiles/faiss.dir/impl/index_read.cpp.o.d
@@ -0,0 +1,304 @@
+faiss/CMakeFiles/faiss.dir/impl/index_read.cpp.o: \
+ /home/cxy/ACORN/ACORN/faiss/impl/index_read.cpp \
+ /usr/include/stdc-predef.h /home/cxy/ACORN/ACORN/faiss/index_io.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/cstdio \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/x86_64-redhat-linux/bits/c++config.h \
+ /usr/include/bits/wordsize.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/x86_64-redhat-linux/bits/os_defines.h \
+ /usr/include/features.h /usr/include/sys/cdefs.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/x86_64-redhat-linux/bits/cpu_defines.h \
+ /usr/include/stdio.h \
+ /opt/rh/devtoolset-9/root/usr/lib/gcc/x86_64-redhat-linux/9/include/stddef.h \
+ /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+ /usr/include/libio.h /usr/include/_G_config.h /usr/include/wchar.h \
+ /opt/rh/devtoolset-9/root/usr/lib/gcc/x86_64-redhat-linux/9/include/stdarg.h \
+ /usr/include/bits/stdio_lim.h /usr/include/bits/sys_errlist.h \
+ /usr/include/bits/stdio.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/string \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/stringfwd.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/memoryfwd.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/char_traits.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/stl_algobase.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/functexcept.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/exception_defines.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/cpp_type_traits.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/ext/type_traits.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/ext/numeric_traits.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/stl_pair.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/move.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/concept_check.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/type_traits \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/stl_iterator_base_types.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/stl_iterator_base_funcs.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/debug/assertions.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/stl_iterator.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/ptr_traits.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/debug/debug.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/predefined_ops.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/postypes.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/cwchar \
+ /usr/include/bits/wchar.h /usr/include/xlocale.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/cstdint \
+ /opt/rh/devtoolset-9/root/usr/lib/gcc/x86_64-redhat-linux/9/include/stdint.h \
+ /usr/include/stdint.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/allocator.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/x86_64-redhat-linux/bits/c++allocator.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/ext/new_allocator.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/new \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/exception \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/exception.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/exception_ptr.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/cxxabi_init_exception.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/typeinfo \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/hash_bytes.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/nested_exception.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/localefwd.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/x86_64-redhat-linux/bits/c++locale.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/clocale \
+ /usr/include/locale.h /usr/include/bits/locale.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/iosfwd \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/cctype /usr/include/ctype.h \
+ /usr/include/endian.h /usr/include/bits/endian.h \
+ /usr/include/bits/byteswap.h /usr/include/bits/byteswap-16.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/ostream_insert.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/cxxabi_forced.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/stl_function.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/backward/binders.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/range_access.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/initializer_list \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/basic_string.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/ext/atomicity.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/x86_64-redhat-linux/bits/gthr.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/x86_64-redhat-linux/bits/gthr-default.h \
+ /usr/include/pthread.h /usr/include/sched.h /usr/include/time.h \
+ /usr/include/bits/sched.h /usr/include/bits/time.h \
+ /usr/include/bits/timex.h /usr/include/bits/pthreadtypes.h \
+ /usr/include/bits/setjmp.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/x86_64-redhat-linux/bits/atomic_word.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/ext/alloc_traits.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/alloc_traits.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/ext/string_conversions.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/cstdlib \
+ /usr/include/stdlib.h /usr/include/bits/waitflags.h \
+ /usr/include/bits/waitstatus.h /usr/include/sys/types.h \
+ /usr/include/sys/select.h /usr/include/bits/select.h \
+ /usr/include/bits/sigset.h /usr/include/sys/sysmacros.h \
+ /usr/include/alloca.h /usr/include/bits/stdlib-float.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/std_abs.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/cerrno /usr/include/errno.h \
+ /usr/include/bits/errno.h /usr/include/linux/errno.h \
+ /usr/include/asm/errno.h /usr/include/asm-generic/errno.h \
+ /usr/include/asm-generic/errno-base.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/functional_hash.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/basic_string.tcc \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/vector \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/stl_construct.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/stl_uninitialized.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/stl_vector.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/stl_bvector.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/vector.tcc \
+ /home/cxy/ACORN/ACORN/faiss/impl/io_macros.h /usr/include/sys/stat.h \
+ /usr/include/bits/stat.h /home/cxy/ACORN/ACORN/faiss/impl/FaissAssert.h \
+ /home/cxy/ACORN/ACORN/faiss/impl/FaissException.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/utility \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/stl_relops.h \
+ /home/cxy/ACORN/ACORN/faiss/impl/platform_macros.h \
+ /home/cxy/ACORN/ACORN/faiss/impl/io.h \
+ /home/cxy/ACORN/ACORN/faiss/Index.h \
+ /home/cxy/ACORN/ACORN/faiss/MetricType.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/sstream \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/istream \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/ios \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/ios_base.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/locale_classes.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/locale_classes.tcc \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/system_error \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/x86_64-redhat-linux/bits/error_constants.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/stdexcept \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/streambuf \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/streambuf.tcc \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/basic_ios.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/locale_facets.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/cwctype \
+ /usr/include/wctype.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/x86_64-redhat-linux/bits/ctype_base.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/streambuf_iterator.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/x86_64-redhat-linux/bits/ctype_inline.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/locale_facets.tcc \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/basic_ios.tcc \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/ostream \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/ostream.tcc \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/istream.tcc \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/sstream.tcc \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/iostream \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/regex \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/algorithm \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/stl_algo.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/algorithmfwd.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/stl_heap.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/stl_tempbuf.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/uniform_int_dist.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/limits \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bitset \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/iterator \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/stream_iterator.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/locale \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/locale_facets_nonio.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/ctime \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/x86_64-redhat-linux/bits/time_members.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/x86_64-redhat-linux/bits/messages_members.h \
+ /usr/include/libintl.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/codecvt.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/locale_facets_nonio.tcc \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/locale_conv.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/unique_ptr.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/tuple \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/array \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/uses_allocator.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/invoke.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/memory \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/stl_raw_storage_iter.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/ext/concurrence.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/shared_ptr.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/shared_ptr_base.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/allocated_ptr.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/refwrap.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/ext/aligned_buffer.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/shared_ptr_atomic.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/atomic_base.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/atomic_lockfree_defines.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/backward/auto_ptr.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/stack \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/deque \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/stl_deque.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/deque.tcc \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/stl_stack.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/map \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/stl_tree.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/stl_map.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/stl_multimap.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/erase_if.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/cstring \
+ /usr/include/string.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/std_function.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/regex_constants.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/regex_error.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/regex_automaton.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/regex_automaton.tcc \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/regex_scanner.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/regex_scanner.tcc \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/regex_compiler.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/regex_compiler.tcc \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/regex.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/regex.tcc \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/regex_executor.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/regex_executor.tcc \
+ /home/cxy/ACORN/ACORN/faiss/utils/hamming.h \
+ /home/cxy/ACORN/ACORN/faiss/utils/Heap.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/climits \
+ /opt/rh/devtoolset-9/root/usr/lib/gcc/x86_64-redhat-linux/9/include/limits.h \
+ /opt/rh/devtoolset-9/root/usr/lib/gcc/x86_64-redhat-linux/9/include/syslimits.h \
+ /usr/include/limits.h /usr/include/bits/posix1_lim.h \
+ /usr/include/bits/local_lim.h /usr/include/linux/limits.h \
+ /usr/include/bits/posix2_lim.h /usr/include/bits/xopen_lim.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/cmath /usr/include/math.h \
+ /usr/include/bits/huge_val.h /usr/include/bits/huge_valf.h \
+ /usr/include/bits/huge_vall.h /usr/include/bits/inf.h \
+ /usr/include/bits/nan.h /usr/include/bits/mathdef.h \
+ /usr/include/bits/mathcalls.h /usr/include/bits/mathinline.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/cassert \
+ /usr/include/assert.h \
+ /home/cxy/ACORN/ACORN/faiss/utils/ordered_key_value.h \
+ /home/cxy/ACORN/ACORN/faiss/utils/hamming-inl.h \
+ /home/cxy/ACORN/ACORN/faiss/invlists/InvertedListsIOHook.h \
+ /home/cxy/ACORN/ACORN/faiss/invlists/InvertedLists.h \
+ /home/cxy/ACORN/ACORN/faiss/Index2Layer.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexFlatCodes.h \
+ /home/cxy/ACORN/ACORN/faiss/impl/DistanceComputer.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexIVF.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/unordered_map \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/hashtable.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/hashtable_policy.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/unordered_map.h \
+ /home/cxy/ACORN/ACORN/faiss/Clustering.h \
+ /home/cxy/ACORN/ACORN/faiss/impl/IDSelector.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/unordered_set \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/unordered_set.h \
+ /home/cxy/ACORN/ACORN/faiss/invlists/DirectMap.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexPQ.h \
+ /home/cxy/ACORN/ACORN/faiss/impl/PolysemousTraining.h \
+ /home/cxy/ACORN/ACORN/faiss/impl/ProductQuantizer.h \
+ /home/cxy/ACORN/ACORN/faiss/impl/Quantizer.h \
+ /home/cxy/ACORN/ACORN/faiss/impl/ProductQuantizer-inl.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexAdditiveQuantizer.h \
+ /home/cxy/ACORN/ACORN/faiss/impl/AdditiveQuantizer.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexFlat.h \
+ /home/cxy/ACORN/ACORN/faiss/impl/LocalSearchQuantizer.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/random \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/random.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/x86_64-redhat-linux/bits/opt_random.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/random.tcc \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/numeric \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/stl_numeric.h \
+ /home/cxy/ACORN/ACORN/faiss/utils/utils.h \
+ /home/cxy/ACORN/ACORN/faiss/impl/ProductAdditiveQuantizer.h \
+ /home/cxy/ACORN/ACORN/faiss/impl/ResidualQuantizer.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexAdditiveQuantizerFastScan.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexFastScan.h \
+ /home/cxy/ACORN/ACORN/faiss/utils/AlignedTable.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexHNSW.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexScalarQuantizer.h \
+ /home/cxy/ACORN/ACORN/faiss/impl/ScalarQuantizer.h \
+ /home/cxy/ACORN/ACORN/faiss/impl/AuxIndexStructures.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/mutex \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/chrono \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/ratio \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/parse_numbers.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/std_mutex.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/unique_lock.h \
+ /home/cxy/ACORN/ACORN/faiss/impl/HNSW.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/queue \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/stl_queue.h \
+ /opt/rh/devtoolset-9/root/usr/lib/gcc/x86_64-redhat-linux/9/include/omp.h \
+ /home/cxy/ACORN/ACORN/faiss/utils/random.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexIVFAdditiveQuantizer.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexIVFAdditiveQuantizerFastScan.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexIVFFastScan.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexIVFFlat.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexIVFPQ.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexIVFPQFastScan.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexIVFPQR.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexIVFSpectralHash.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexLSH.h \
+ /home/cxy/ACORN/ACORN/faiss/VectorTransform.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexLattice.h \
+ /home/cxy/ACORN/ACORN/faiss/impl/lattice_Zn.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexNNDescent.h \
+ /home/cxy/ACORN/ACORN/faiss/impl/NNDescent.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexNSG.h \
+ /home/cxy/ACORN/ACORN/faiss/impl/NSG.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexPQFastScan.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexPreTransform.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexRefine.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexRowwiseMinMax.h \
+ /home/cxy/ACORN/ACORN/faiss/MetaIndexes.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexIDMap.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexBinary.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexReplicas.h \
+ /home/cxy/ACORN/ACORN/faiss/impl/ThreadedIndex.h \
+ /home/cxy/ACORN/ACORN/faiss/utils/WorkerThread.h \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/condition_variable \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/future \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/thread \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/atomic \
+ /opt/rh/devtoolset-9/root/usr/include/c++/9/bits/atomic_futex.h \
+ /home/cxy/ACORN/ACORN/faiss/impl/ThreadedIndex-inl.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexShards.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexBinaryFlat.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexBinaryFromFloat.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexBinaryHNSW.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexBinaryHash.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexBinaryIVF.h \
+ /home/cxy/ACORN/ACORN/faiss/IndexACORN.h \
+ /home/cxy/ACORN/ACORN/faiss/impl/ACORN.h /usr/include/sys/time.h
